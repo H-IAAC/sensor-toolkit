c87c85c852641ef622b120fb4db7c0f78632198a
felipe.augusto <felipe.augusto@eldorado.org.br>
2022-05-23 12:37:54 -0300

    Testing jitpack

--- Clients/HIAAC/app/src/main/java/br/org/eldorado/hiaac/datacollector/DataCollectorActivity.java
+++ Clients/HIAAC/app/src/main/java/br/org/eldorado/hiaac/datacollector/DataCollectorActivity.java
@@ -83,11 +83,9 @@ public class DataCollectorActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
