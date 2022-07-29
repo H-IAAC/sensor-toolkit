28900b98fc93c7fabe13b0de9697aa6db3efb5b2
Daniel Adam Miranda <daniel.miranda@eldorado.org.br>
2022-07-29 16:08:48 -0300

    Simplify GpsController

--- HiaacLib/app/src/main/java/br/org/eldorado/hiaac/actuators/GpsController.java
+++ HiaacLib/app/src/main/java/br/org/eldorado/hiaac/actuators/GpsController.java
@@ -2,7 +2,6 @@ package br.org.eldorado.hiaac.actuators;
 
 import static android.content.ContentValues.TAG;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.IntentSender;
@@ -17,11 +16,9 @@ import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResponse;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 
 public class GpsController {
     private Context context;
@@ -29,6 +26,7 @@ public class GpsController {
     private LocationSettingsRequest mLocationSettingsRequest;
     private LocationManager locationManager;
     private LocationRequest locationRequest;
+
     protected GpsController(Context context) {
         this.context = context;
         locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
@@ -42,51 +40,34 @@ public class GpsController {
         mLocationSettingsRequest = builder.build();
         builder.setAlwaysShow(true); //this is the key ingredient
     }
+
     // method for turn on GPS
-    public void turnGPSOn(onGpsListener onGpsListener) {
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            if (onGpsListener != null) {
-                onGpsListener.gpsStatus(true);
-            }
-        } else {
-            mSettingsClient
-                    .checkLocationSettings(mLocationSettingsRequest)
-                    .addOnSuccessListener((Activity) context, new OnSuccessListener<LocationSettingsResponse>() {
-                        @SuppressLint("MissingPermission")
-                        @Override
-                        public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
-                        //  GPS is already enable, callback GPS status through listener
-                            if (onGpsListener != null) {
-                                onGpsListener.gpsStatus(true);
-                            }
+    public void turnGPSOn() {
+
+        mSettingsClient
+                .checkLocationSettings(mLocationSettingsRequest)
+                .addOnFailureListener((Activity) context, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        int statusCode = ((ApiException) e).getStatusCode();
+                        switch (statusCode) {
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                try {
+                                    // Show the dialog by calling startResolutionForResult(), and check the
+                                    // result in onActivityResult().
+                                    ResolvableApiException rae = (ResolvableApiException) e;
+                                    rae.startResolutionForResult((Activity) context, 1000);
+                                } catch (IntentSender.SendIntentException sie) {
+                                    Log.i(TAG, "PendingIntent unable to execute request.");
+                                }
+                                break;
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                String errorMessage = "Location settings are inadequate, and cannot be " +
+                                        "fixed here. Fix in Settings.";
+                                Log.e(TAG, errorMessage);
+                                Toast.makeText((Activity) context, errorMessage, Toast.LENGTH_LONG).show();
                         }
-                    })
-                    .addOnFailureListener((Activity) context, new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            int statusCode = ((ApiException) e).getStatusCode();
-                            switch (statusCode) {
-                                case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
-                                    try {
-                                        // Show the dialog by calling startResolutionForResult(), and check the
-                                        // result in onActivityResult().
-                                        ResolvableApiException rae = (ResolvableApiException) e;
-                                        rae.startResolutionForResult((Activity) context, 1000);
-                                    } catch (IntentSender.SendIntentException sie) {
-                                        Log.i(TAG, "PendingIntent unable to execute request.");
-                                    }
-                                    break;
-                                case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
-                                    String errorMessage = "Location settings are inadequate, and cannot be " +
-                                            "fixed here. Fix in Settings.";
-                                    Log.e(TAG, errorMessage);
-                                    Toast.makeText((Activity) context, errorMessage, Toast.LENGTH_LONG).show();
-                            }
-                        }
-                    });
-        }
-    }
-    public interface onGpsListener {
-        void gpsStatus(boolean isGPSEnable);
+                    }
+                });
     }
 }
