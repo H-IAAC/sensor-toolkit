2212ac2ab94089b0940f23c8e30a2bcf61160055
Saulo A. Moraes <saulo.moraes@eldorado.org.br>
2024-04-22 14:48:07 -0300

    Fix schedule to always show message, even when user set it to next day

--- HiaacLib/app/src/main/java/br/org/eldorado/hiaac/datacollector/util/AlarmConfig.java
+++ HiaacLib/app/src/main/java/br/org/eldorado/hiaac/datacollector/util/AlarmConfig.java
@@ -15,6 +15,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import br.org.eldorado.hiaac.R;
 import br.org.eldorado.hiaac.datacollector.DataCollectorActivity;
@@ -88,22 +89,26 @@ public class AlarmConfig {
 
             Calendar now = Calendar.getInstance();
             now.setTimeInMillis(SensorSDK.getInstance().getRemoteTime());
-            if (c.after(now)) {
-                acquireWakeLock();
 
-                log.i("Scheduler: " + labelConfig.experiment + " to start at [" + c.getTime() + "] id: " + labelConfig.id);
+            // If configured to saved after the current date, then set the scheduler to next day
+            if (!c.after(now))
+                labelConfig.scheduledTime = labelConfig.scheduledTime + TimeUnit.HOURS.toMillis(24);
 
-                long startsTime = labelConfig.scheduledTime - SensorSDK.getInstance().getRemoteTime() - 7000;
-                mgr.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP,
-                                              SensorSDK.getInstance().getRemoteTime() + startsTime,
-                                              pendingAlarm);
+            acquireWakeLock();
 
-                isConfigured = true;
-                idConfigured = labelConfig.id;
+            log.i("Scheduler: " + labelConfig.experiment + " to start at [" + c.getTime() + "] id: " + labelConfig.id);
 
-                setScheduler(c.getTime());
-                return c.getTime();
-            }
+            long startsTime = labelConfig.scheduledTime - SensorSDK.getInstance().getRemoteTime() - 7000;
+
+            mgr.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP,
+                    SensorSDK.getInstance().getRemoteTime() + startsTime,
+                    pendingAlarm);
+
+            isConfigured = true;
+            idConfigured = labelConfig.id;
+
+            setScheduler(c.getTime());
+            return c.getTime();
         }
 
         setScheduler(null);
