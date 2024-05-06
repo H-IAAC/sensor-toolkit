46dee06153eb035c64e6c5b7c94c2469d6ca8a68
Saulo A. Moraes <saulo.moraes@eldorado.org.br>
2024-05-06 11:19:22 -0300

    SchedulerReceiver cleanup

--- HiaacLib/app/src/main/java/br/org/eldorado/hiaac/datacollector/receiver/SchedulerReceiver.java
+++ HiaacLib/app/src/main/java/br/org/eldorado/hiaac/datacollector/receiver/SchedulerReceiver.java
@@ -1,29 +1,16 @@
 package br.org.eldorado.hiaac.datacollector.receiver;
 
-import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.ServiceConnection;
-import android.os.CountDownTimer;
 import android.os.Handler;
-import android.os.IBinder;
-import android.view.Gravity;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import br.org.eldorado.hiaac.R;
 import br.org.eldorado.hiaac.datacollector.DataCollectorActivity;
-import br.org.eldorado.hiaac.datacollector.model.DataTrack;
-import br.org.eldorado.hiaac.datacollector.service.ExecutionService;
 import br.org.eldorado.hiaac.datacollector.util.Log;
 import br.org.eldorado.hiaac.datacollector.util.Utils;
 import br.org.eldorado.hiaac.datacollector.util.WakeLocks;
 import br.org.eldorado.hiaac.datacollector.util.AlarmConfig;
 import br.org.eldorado.hiaac.datacollector.view.adapter.LabelRecyclerViewAdapter;
-
-import br.org.eldorado.sensorsdk.SensorSDK;
 
 public class SchedulerReceiver extends BroadcastReceiver {
     private static final Log log = new Log("SchedulerReceiver");
